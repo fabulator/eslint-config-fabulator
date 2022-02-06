ab02ea9a561d4a66c7fcfc80c1c024d8bf5c2c48
Michal Ozogán <michal@ozogan.eu>
2022-02-06 10:30:31 +0000

    Changed: Update dependencies

--- tests/validate-config.test.js
+++ tests/validate-config.test.js
@@ -1,20 +1,19 @@
-const eslint = require('eslint');
+const { ESLint } = require('eslint');
 const fs = require('fs');
 
 function getErrors(configFile) {
-    const { CLIEngine } = eslint;
-
-    const cli = new CLIEngine({
-        configFile,
+    const cli = new ESLint({
+        overrideConfigFile: configFile,
     });
 
-    return cli.executeOnText(fs.readFileSync('./jest.js', 'utf8'));
+    return cli.lintText(fs.readFileSync('./jest.js', 'utf8'));
 }
 
 describe('Validate configs by eslint', () => {
     ['index.js', 'react.js', 'jest.js'].forEach((file) => {
-        it(`load config ${file} in eslint to validate all rule syntax is correct`, () => {
-            expect(getErrors(file).results[0].messages).toEqual([]);
+        it(`load config ${file} in eslint to validate all rule syntax is correct`, async () => {
+            const results = await getErrors(file);
+            expect(results[0].messages).toEqual([]);
         });
     });
 });
