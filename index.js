ed6ada0cc4173e54ff2413f341bf48beb3fe24c2
Michal Ozogán <michal@ozogan.eu>
2025-03-20 16:13:30 +0100

    Added: Vitest rules

--- index.js
+++ index.js
@@ -8,14 +8,63 @@ import importPlugin from 'eslint-plugin-import';
 import tseslint from 'typescript-eslint';
 import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
+import vitest from '@vitest/eslint-plugin';
 
+// eslint-disable-next-line import/no-default-export
 export default defineConfig([
   {
     extends: [arrayFunc.configs.recommended],
     rules: { 'array-func/prefer-array-from': 0 },
   },
-  js.configs.recommended,
-  importPlugin.flatConfigs.recommended,
+  {
+    extends: [js.configs.recommended],
+    rules: { 'no-irregular-whitespace': 'off' },
+  },
+  {
+    files: ['**/*.test.ts', '**/*.test.js', '**/*.test.jsx', '**/*.test.tsx'],
+    plugins: {
+      vitest,
+    },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+  },
+  {
+    extends: [importPlugin.flatConfigs.recommended],
+    rules: {
+      'import/no-deprecated': 'error',
+      'import/no-empty-named-blocks': 'error',
+      'import/no-extraneous-dependencies': [
+        'error',
+        {
+          devDependencies: [
+            '**/*.config.js',
+            '**/*.config.ts',
+            '**/*.setup.ts',
+            '**/*.test.ts',
+            '**/*.test.tsx',
+            '**/lang/**/*.*',
+          ],
+        },
+      ],
+      'import/no-mutable-exports': 'error',
+      'import/no-unused-modules': 'error',
+      'import/no-cycle': 'error',
+      'import/no-relative-packages': 'error',
+      'import/no-self-import': 'error',
+      'import/order': ['error', { 'newlines-between': 'never' }],
+      'import/no-default-export': 'error',
+      'import/newline-after-import': 'error',
+      'import/first': 'error',
+    },
+  },
+  {
+    files: ['**/*.config.*s'],
+    rules: {
+      'import/no-default-export': 'off',
+    },
+  },
   {
     plugins: {
       canonical: canonical,
@@ -23,43 +72,65 @@ export default defineConfig([
   },
   {
     extends: [reactHooks.configs['recommended-latest']],
-    files: ['use*.*sx', 'use*.js', 'use*.ts', '*.tsx', '*.jsx'],
+    files: ['**/use*.*sx', '**/use*.js', '**/use*.ts', '**/*.tsx', '**/*.jsx'],
     rules: {
       'react-hooks/exhaustive-deps': 'error',
       'react-hooks/rules-of-hooks': 'error',
     },
   },
   {
-    extends: [reactPlugin.configs.recommended],
-    files: ['*.tsx', '*.jsx'],
+    extends: [
+      reactPlugin.configs.flat.recommended,
+      jsxA11y.flatConfigs.recommended,
+    ],
+    files: ['**/*.tsx', '**/*.jsx'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/display-name': 'off',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
   },
   {
     extends: [
       tseslint.configs.strictTypeChecked,
       tseslint.configs.stylisticTypeChecked,
     ],
-    files: ['*.tsx', '*.ts'],
+    files: ['**/*.tsx', '**/*.ts'],
     rules: {
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/restrict-template-expressions': [
+        'error',
+        {
+          allowAny: false,
+          allowBoolean: false,
+          allowNullish: false,
+          allowNumber: true,
+          allowRegExp: false,
+          allowNever: false,
+        },
+      ],
       '@typescript-eslint/no-non-null-asserted-optional-chain': 'off',
       '@typescript-eslint/no-non-null-assertion': 'off',
       '@typescript-eslint/no-empty-function': 'off',
-      '@typescript-eslint/no-unused-vars': [
-        'error',
-        {
-          args: 'all',
-          argsIgnorePattern: '^_',
-          ignoreRestSiblings: true,
-          vars: 'all',
-        },
-      ],
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-empty-object-type': 'off',
       '@typescript-eslint/non-nullable-type-assertion-style': 'error',
       '@typescript-eslint/prefer-optional-chain': 'error',
+      '@typescript-eslint/prefer-nullish-coalescing': 0,
     },
   },
   {
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: { ecmaVersion: 'latest' },
+    },
+    linterOptions: {
+      reportUnusedInlineConfigs: 'error',
+      reportUnusedDisableDirectives: 'error',
     },
     settings: {
       'import/resolver': {
